aadb5a2d09ad7a55d7e163756304fc4ad5cdba20
Victor Vieira <victorvieira89@gmail.com>
2022-09-29 21:52:12 -0300

    fix(repositories): add content to  repository and  repository

---
+++ src/main/java/br/ufrn/wallet/repository/ExpenseRepository.java
@@ -0,0 +1,8 @@
+package br.ufrn.wallet.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufrn.wallet.model.Expense;
+
+public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
+}
